2d130841846ad954d1da430bb0ed993df21674e1
hoangdev <hoangdev@hoangdevs-iMac.local>
2024-01-20 00:20:49 +0900

    update

--- src/utils/Strings.js
+++ src/utils/Strings.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://192.168.0.116';
-export const PORT = ':5000';
+export const BASE_URL = 'http://126.31.242.212';
+export const PORT = ':8000';
 export const USER_URL = '/users';
 export const DEPARTMENT = '/departments';
 export const LOGIN_URL = '/login';
