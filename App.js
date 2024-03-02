8b95f0b477a59062cdcb444429c68c3b074a5194
hoangdev <hoangdev@hoangdevs-iMac.local>
2024-03-02 20:50:33 +0900

    update push notification on android

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import {View, Text, SafeAreaView} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import MainNavigator from './src/navigation/MainNavigator';
 import MyStore from './src/redux/MyStore';
+import {
+  NotificationServices,
+  requestUserPermission,
+} from './src/utils/notification/PushNotifications';
 
 const App = () => {
+  useEffect(() => {
+    requestUserPermission();
+    NotificationServices();
+  }, []);
   return (
     <Provider store={MyStore}>
       <MainNavigator />
