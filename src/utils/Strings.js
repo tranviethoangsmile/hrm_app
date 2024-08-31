ca7dc2c664d7e82131e896b5285f3862148ef24b
hoangdev <hoangdev@hoangdevs-iMac.local>
2024-08-31 21:20:15 +0900

    update

--- src/utils/Strings.js
+++ src/utils/Strings.js
@@ -1,7 +1,7 @@
-// export const BASE_URL = 'http://18.236.164.13';
-// export const PORT = ':80';
-export const BASE_URL = 'http://localhost';
-export const PORT = ':4000';
+export const BASE_URL = 'http://18.236.164.13';
+export const PORT = ':80';
+// export const BASE_URL = 'http://localhost';
+// export const PORT = ':4000';
 export const API = '/api';
 export const VERSION = '/version';
 export const V1 = '/v1';
